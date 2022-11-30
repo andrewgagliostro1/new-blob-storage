3c686b12cdf528441be18d293be5e3b924b8736f
US\agagliostro <agagliostro@kpmg.com>
2022-11-30 12:17:54 -0500

    built

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AzureBlobClient = void 0;
 const identity_1 = require("@azure/identity");
@@ -61,6 +68,75 @@ class AzureBlobClient {
             catch (e) {
                 console.log(e);
                 throw e;
+            }
+        });
+    }
+    listBlobHierarchical(containerName, hierarchyDelimiter) {
+        var _a, e_1, _b, _c, _d, e_2, _e, _f;
+        return __awaiter(this, void 0, void 0, function* () {
+            // page size - artificially low as example
+            const maxPageSize = 2;
+            // some options for filtering list
+            const listOptions = {
+                includeMetadata: true,
+                includeSnapshots: false,
+                includeTags: true,
+                includeVersions: false,
+                prefix: ''
+            };
+            let i = 1;
+            console.log(`Folder $ /`);
+            let containerClient = this.blob_service_client.getContainerClient(containerName);
+            try {
+                for (var _g = true, _h = __asyncValues(containerClient
+                    .listBlobsByHierarchy('/', listOptions)
+                    .byPage({ maxPageSize })), _j; _j = yield _h.next(), _a = _j.done, !_a;) {
+                    _c = _j.value;
+                    _g = false;
+                    try {
+                        const response = _c;
+                        console.log(`   Page ${i++}`);
+                        const segment = response.segment;
+                        if (segment.blobPrefixes) {
+                            try {
+                                // Do something with each virtual folder
+                                for (var _k = true, _l = (e_2 = void 0, __asyncValues(segment.blobPrefixes)), _m; _m = yield _l.next(), _d = _m.done, !_d;) {
+                                    _f = _m.value;
+                                    _k = false;
+                                    try {
+                                        const prefix = _f;
+                                        // build new virtualHierarchyDelimiter from current and next
+                                        yield this.listBlobHierarchical(containerName, `${hierarchyDelimiter}${prefix.name}`);
+                                    }
+                                    finally {
+                                        _k = true;
+                                    }
+                                }
+                            }
+                            catch (e_2_1) { e_2 = { error: e_2_1 }; }
+                            finally {
+                                try {
+                                    if (!_k && !_d && (_e = _l.return)) yield _e.call(_l);
+                                }
+                                finally { if (e_2) throw e_2.error; }
+                            }
+                        }
+                        for (const blob of response.segment.blobItems) {
+                            // Do something with each blob
+                            console.log(`\tBlobItem: name - ${blob.name}`);
+                        }
+                    }
+                    finally {
+                        _g = true;
+                    }
+                }
+            }
+            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+            finally {
+                try {
+                    if (!_g && !_a && (_b = _h.return)) yield _b.call(_h);
+                }
+                finally { if (e_1) throw e_1.error; }
             }
         });
     }
